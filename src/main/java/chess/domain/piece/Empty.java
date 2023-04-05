e8ea2a28004d563f7e4cb9631f14653c4a6cf5d5
kevstevie <jianna442@gmail.com>
2023-04-05 13:05:18 +0900

    feat: 빈칸 null 패턴으로 사용

---
+++ src/main/java/chess/domain/piece/Empty.java
@@ -0,0 +1,32 @@
+package chess.domain.piece;
+
+import chess.domain.position.Position;
+
+import java.util.List;
+
+public final class Empty implements Piece {
+
+    private static final Empty EMPTY = new Empty();
+
+    private Empty() {
+    }
+
+    public static Empty getInstance() {
+        return EMPTY;
+    }
+
+    @Override
+    public List<Position> computeMovablePositions(final Position target) {
+        throw new IllegalArgumentException("빈칸은 이동할 수 없습니다.");
+    }
+
+    @Override
+    public boolean isSamePosition(final Position source) {
+        throw new IllegalArgumentException("빈칸은 이동할 수 없습니다.");
+    }
+
+    @Override
+    public Piece move(final Position target) {
+        throw new IllegalArgumentException("빈칸은 이동할 수 없습니다.");
+    }
+}
